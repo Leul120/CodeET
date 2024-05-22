add6a74a9c04adbd80a6c344707e3997ce53fd5e
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-05-22 15:28:38 -0700

    Create payment.js

---
+++ server/utils/payment.js
@@ -0,0 +1,106 @@
+/* 
+    CHAPA API PAYMENT INTEGRATION TEST
+    Required: Chapa secret key || GET THE KEY BY REGISTERING @ https://dashboard.chapa.co/register
+*/
+
+const Course=require('../models/CourseModel')
+const User=require('../models/UserModel')
+const axios=require('axios')
+
+const CHAPA_URL = "https://api.chapa.co/v1/transaction/initialize"
+const CHAPA_AUTH = process.env.CHAPA_AUTH // || register to chapa and get the key
+
+
+// req header with chapa secret key
+const config = {
+    headers: {
+        Authorization: `Bearer ${CHAPA_AUTH}`
+    }
+}
+
+exports.pay= async (req, res) => {
+        const courseID=req.params.courseID
+        // const course=await Course.findById(courseID)
+        
+        const userID=req.params.userID
+        console.log(userID)
+        console.log(courseID)
+        // const user=await User.findById(req.params.userID)
+         // chapa redirect you to this url when payment is successful
+        const CALLBACK_URL = `http://localhost:8000/api/verify-payment/${courseID}/${userID}/`
+        const RETURN_URL = `http://localhost:3000`
+
+        // a unique reference given to every transaction
+        const TEXT_REF = "tx-myecommerce12345-" + Date.now()
+        const {amount,email,first_name,last_name}=req.body
+        // form data
+        const data = {
+            amount:amount, 
+            currency: "ETB",
+            email: email,
+            first_name: first_name,
+            last_name: last_name,
+            tx_ref: TEXT_REF,
+            callback_url: CALLBACK_URL + TEXT_REF,
+            return_url: RETURN_URL,
+           
+        }
+
+        console.log(data)
+        try{
+        const response=await axios.post("https://api.chapa.co/v1/transaction/initialize", data, {
+    headers: {
+        Authorization: "Bearer CHASECK_TEST-EqQOEHfuqjYtcueTtuMht1oMicnQNczz"
+    },
+    timeout: 10000 // increase timeout to 10 seconds
+})
+        console.log(response)
+        const responsed=response.data
+        console.log(response.data.data.checkout_url)
+        res.status(200).json({
+            responsed
+            
+    })}catch(err) {
+        if (err.response && err.response.data) {
+            const { message, status, data } = err.response.data;
+            console.error('Chapa API Error:', message, status, data);
+            // Provide a user-friendly error message or handle the error accordingly
+        } else {
+            console.error('Network Error:', err.message);
+        }
+    };
+}
+
+// verification endpoint
+exports.verifyPayment= async (req, res) => {
+        const courseID=req.params.courseID.toString()
+        const userID=req.params.userID.toString()
+        console.log("verifying")
+        //verify the transaction 
+        await axios.get("https://api.chapa.co/v1/transaction/verify/" + req.params.id,{
+            headers: {
+                Authorization: "Bearer CHASECK_TEST-EqQOEHfuqjYtcueTtuMht1oMicnQNczz"
+            }
+        } )
+            .then(async (response) => {
+                const responsed=response.data
+                console.log("Payment was successfully verified")
+                await User.findByIdAndUpdate(userID, { $push: { courses: courseID } });
+                await Course.findByIdAndUpdate(courseID, { $inc: { bought: 1 } });
+                res.status(200).json({
+                    status:"success",
+                    responsed
+                })
+            }) 
+            .catch((err) => console.log("Payment can't be verfied", err))
+}
+exports.success=(req,res)=>{
+    res.status(200).json({
+        status:"Success"
+    })
+}
+// app.get("/api/payment-success", async (req, res) => {
+//     res.render("success")
+// })
+
+
