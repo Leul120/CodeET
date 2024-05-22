91302fae07772b83c8c4e869c1680f9e7788771a
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-05-22 15:29:06 -0700

    Create CourseModel.js

---
+++ server/models/CourseModel.js
@@ -0,0 +1,38 @@
+const mongoose=require('mongoose')
+const CourseSchema=new mongoose.Schema({
+    Title:{
+        type:String,
+        required:[true,'must provide name'],
+        trim:true,
+    },
+    Year:Number,
+    Rating:Number,
+    Released:Date,
+    Size:String,
+    Description:{
+        type:String,
+        
+        trim:true,
+    },
+    IntroLink:String,
+    Language:String,
+    Poster:String,
+    bought:{type:Number,
+        default:0},
+    VideoLink:{type:[String],
+        select:false},
+    Price:Number,
+    Difficulty:String,
+    Learn:String,
+    Requirements:String,
+    CourseFor:String,
+    DTitle:String,
+    CourseGoal:String
+
+})
+CourseSchema.pre(/^find/,function(next){
+    this.find({Response:{$ne:true}
+    })
+    next()
+})
+module.exports=mongoose.model("Course",CourseSchema,'courses')
