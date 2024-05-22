075287a767dca0c70e3c64ecd488857dd1887711
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-05-22 15:28:55 -0700

    Create courseRoute.js

---
+++ server/routes/courseRoute.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { getAllCourses, PostCourses, getCourse, updateCourse, deleteCourse, aliasTopCourses, getCourseStat } = require('../controllers/courseController');
+const {protect, restrictTo, checkEnrollment}=require('../controllers/authController');
+const { getCheckoutSession, verifyPayment, pay, success } = require('../utils/payment');
+// const attributes=/\b(_id|Title||lt)\b/g
+// console.log(attributes)
+const router = express.Router();
+router.route('/Top-5-Courses').get(aliasTopCourses,getAllCourses)
+router.route('/course-stat').get(getCourseStat)
+
+router.route('/course').get(getAllCourses).post(PostCourses);
+router.route('/:courseID').get(protect,getCourse).patch(updateCourse).delete(protect,restrictTo('admin'),deleteCourse);
+
+router.route('/checkEnrollment/:courseID').get(checkEnrollment)
+
+router.route("/pay/:courseID/:userID").post(pay)
+router.route("/verify-payment/:courseID/:userID/:id").get(verifyPayment)
+router.route("/payment-success").get(success)
+module.exports = router;
