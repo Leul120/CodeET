05605e8ff3a7cce7dded57673c3b76809f116ef9
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-05-22 23:40:46 -0700

    Delete client directory

--- client/src/Enroll.js
+++
@@ -1,6 +0,0 @@
-export const handleFreeEnrollment=()=>{
-
-}
-export const handlePaidEnrollment=()=>{
-    
-}
--- client/src/Validation/UserValidation.js
+++
@@ -1,6 +0,0 @@
-import * as yup from 'yup'
-export const userSchema=yup.object().shape({
-    name:yup.string().required('Please Enter A Full Name'),
-    email:yup.string().email('Please Enter A Valid Email').required('Please Enter An Email'),
-    password:yup.string().min(8).required('Please Enter A Password'),
-    passwordConfirm:yup.string().required('Please Confirm Your Password')}).oneOF([yup.ref("password"),null])
