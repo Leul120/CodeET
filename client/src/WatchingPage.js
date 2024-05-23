f895851d277f73780e00bcd6565256edca2ee3e4
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-05-23 01:39:58 -0700

    Update WatchingPage.js

--- client/src/WatchingPage.js
+++ client/src/WatchingPage.js
@@ -27,7 +27,7 @@ const WatchingPage = () => {
     console.log(token)
     const filter=async ()=>{
       
-      const response=await axios.get(`http://localhost:8000/api/${courseID}`,{withCredentials: true,
+      const response=await axios.get(`${process.env.URL}/api/${courseID}`,{withCredentials: true,
         headers:{
           Authorization:`Bearer ${token}`
         }}
