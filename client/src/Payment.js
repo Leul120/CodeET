5bcc23c5771092fbc3dc3ed6cb18ed63c72f26e9
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-05-23 06:51:00 -0700

    Update Payment.js

--- client/src/Payment.js
+++ client/src/Payment.js
@@ -45,17 +45,16 @@ const paymentData=async (data)=>{
 }
 console.log(course)
   return (
-    <div className='flex justify-center pt-44 bg-gradient-to-b from-slate-200 to-slate-950 h-screen pb-44'>
-        <form onSubmit={handleSubmit(paymentData)} className='flex flex-col justify-center bg-gradient-to-t rounded-md  p-10 mt-3 w-72 items-center gap-3'>
+    <div className='flex justify-center pt-44 bg-gradient-to-b from-slate-200 to-slate-700 h-screen pb-44'>
+        <form onSubmit={handleSubmit(paymentData)} className='flex flex-col justify-center bg-gradient-to-t from-slate-200 to-slate-500 rounded-md  p-10 mt-3 w-72 items-center gap-3 shadow-lg shadow-slate-800'>
             <input type='text' className="text-black h-9 mt-1 pl-2 rounded-md " placeholder='First Name' {...register('first_name')}/>
             <input type='text' className="text-black h-9 mt-1 pl-2 rounded-md " placeholder='Last Name' {...register('last_name')}/>
             <input type='text' className="text-black h-9 mt-1 pl-2 rounded-md " placeholder='email' {...register('email')}/>
             <div className='border flex flex-row items-center h-9 bg-white w-56 rounded-lg'>
             <p className=' text-black h-full rounded-lg pl-2 bg-white flex items-center'>Amount :</p>
-            <input type='text' className="text-black h-9  w-16 border-0 ring-0 outline-0 rounded-md "  {...register('amount')} placeholder='amount' prefix="amount : " value={course.Price}/>
+            <input type='text' className="text-black h-9   w-16 border-0 ring-0 outline-0 rounded-md "  {...register('amount')} placeholder='amount' prefix="amount : " value={course.Price}/>
             </div>
-            <input type="submit" value="submit"  className=' bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 rounded-md h-8 focus:ring-green-300 focus:ring-4 focus:outline-none hover:ring-white w-full'/>
-            
+            <button type="submit"  className='w-full h-8 text-white flex items-center justify-center bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'>Pay</button>
         </form>
     </div>
   )
