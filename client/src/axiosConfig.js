39cae532cbfdf6fa9b25463fd005c26be3f9ab56
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-05-23 01:28:13 -0700

    Create axiosConfig.js

---
+++ client/src/axiosConfig.js
@@ -0,0 +1,13 @@
+// import axios from 'axios';
+// import https from 'https';
+
+// const agent = new https.Agent({
+//   protocol: 'https:',
+//   rejectUnauthorized: false, // Only include this option if necessary
+//   // Other options as needed
+// });
+
+// axios.defaults.httpAgent = agent;
+// axios.defaults.httpsAgent = agent;
+
+// export default axios
