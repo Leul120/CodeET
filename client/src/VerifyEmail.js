8dc4de3ede8b07b984299a93baee911ed1ab6d56
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-05-23 21:16:49 -0700

    Update VerifyEmail.js

--- client/src/VerifyEmail.js
+++ client/src/VerifyEmail.js
@@ -1,16 +1,16 @@
-import React, { useContext, useState } from 'react'
-import { AppContext } from './App'
-import { useNavigate, useSearchParams } from 'react-router-dom'
+// import React, { useContext, useState } from 'react'
+// import { AppContext } from './App'
+// import { useNavigate, useSearchParams } from 'react-router-dom'
 
-const VerifyEmail = () => {
-    const {user,updateUser,isLoading,seIsLoading}=useContext(AppContext)
-    const [error,setError]=useState(false)
-    const [searchParams,setSearchParams]=useSearchParams()
-    const navigate=useNavigate()
-    const emailToken=searchParams.get("emailToken")
-  return (
-    <div>VerifyEmail</div>
-  )
-}
+// const VerifyEmail = () => {
+//     const {user,updateUser,isLoading,seIsLoading}=useContext(AppContext)
+//     const [error,setError]=useState(false)
+//     const [searchParams,setSearchParams]=useSearchParams()
+//     const navigate=useNavigate()
+//     const emailToken=searchParams.get("emailToken")
+//   return (
+//     <div>VerifyEmail</div>
+//   )
+// }
 
-export default VerifyEmail
+// export default VerifyEmail
