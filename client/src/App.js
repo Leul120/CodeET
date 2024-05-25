718b29ce300a6587819166392f147b50c36c3ab5
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-05-25 01:57:11 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,64 +1,4 @@
 
-// import axios from 'axios';
-// import './App.css';
-// import { useEffect, useState } from 'react';
-// import {BrowserRouter as Router ,Routes,Route} from 'react-router-dom'
-// import Home from './Home';
-// import Navbar from './Navbar';
-// import { createContext } from 'react';
-// import SingleCourse from './SingleCourse';
-// import { QueryClient,QueryClientProvider,useQuery } from 'react-query';
-// import WatchingPage from './WatchingPage';
-
-// export const AppContext=createContext()
-
-// function App() {
-//   const client=new QueryClient({})
-//   const [text,setText]=useState("")
-//   const [courses,setCourses]=useState([])
-//   // async function fetchData() {
-//   //   try {
-//   //     const response = await axios.get('http://localhost:8000/api/course');
-//   //     // console.log(response.data.courses);
-      
-//   //   } catch (error) {
-//   //     console.error(error);
-//   //   }
-//   // }
-//   // useEffect(()=>{
-//   //   fetchData();
-//   // },[])
-//   const { data, isLoading, isFetched, error, refetch } = useQuery('courses', {
-//     queryFn: async () => {
-//       const res = await axios.get('http://localhost:8000/api/course');
-//       setCourses(res.data.courses)
-//       return res.data.courses
-
-//     },
-//     enabled: true // Fetch data only when needed
-//   });
-//   console.log(courses)
-//   return (
-//     <div className="App">
-//      <AppContext.Provider value={{courses,text,setText}}>
-//      <QueryClientProvider client={client}>
-//       <Router>
-//       <Navbar/>
-//         <Routes>
-//           <Route path='/' element={<Home/>}/>
-//           <Route path='/course/:courseID' element={<SingleCourse/>}/>
-//           <Route path='course/watch' element={<WatchingPage/>}/>
-//         </Routes>
-//       </Router>
-//       </QueryClientProvider>
-//      </AppContext.Provider >
-
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import './App.css';
 import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -92,10 +32,11 @@ function App() {
   const [course, setCourse] = useState({});
   const [isLoading,setIsLoading]=useState(true)
   const [sort,setSort]=useState("-Released")
+  const [menu,setMenu]=useState('home')
   return (
     <div className="App">
       <QueryClientProvider client={client}>
-        <AppContext.Provider value={{ courses, text, setText ,setCourses,count,setCount,page,setPage,isLoading,setIsLoading,tokens,setTokens,name,setName,user,setUser,enrolled,setEnrolled,course,setCourse,refetcher,setRefetcher,setSort,sort}}>
+        <AppContext.Provider value={{ courses, text, setText ,setCourses,count,setCount,page,setPage,isLoading,setIsLoading,tokens,setTokens,name,setName,user,setUser,enrolled,setEnrolled,course,setCourse,refetcher,setRefetcher,setSort,sort,menu,setMenu}}>
           <Router>
             <Navbar />
             <Routes>
