cd9d02fc3ddf191d912ee4dd4edfd573f62ce4b7
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-05-25 01:57:46 -0700

    Update courseRoute.js

--- server/routes/courseRoute.js
+++ server/routes/courseRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllCourses, PostCourses, getCourse, updateCourse, deleteCourse, aliasTopCourses, getCourseStat,dashboard } = require('../controllers/courseController');
+const { getAllCourses, PostCourses, getCourse, updateCourse, deleteCourse, aliasTopCourses, getCourseStat,dashboard,sendEmail} = require('../controllers/courseController');
 const {protect, restrictTo}=require('../controllers/authController');
 const { getCheckoutSession, verifyPayment, pay, success } = require('../utils/payment');
 // const attributes=/\b(_id|Title||lt)\b/g
@@ -7,13 +7,14 @@ const { getCheckoutSession, verifyPayment, pay, success } = require('../utils/pa
 const router = express.Router();
 router.route('/Top-5-Courses').get(aliasTopCourses,getAllCourses)
 router.route('/course-stat').get(getCourseStat)
-
+router.route("/sendEmail").get(sendEmail)
 router.route('/course').get(getAllCourses).post(PostCourses);
 router.route('/:courseID').get(protect,getCourse).patch(updateCourse).delete(protect,restrictTo('admin'),deleteCourse);
 
 
 router.route('/dashboard/:userID').get(protect,dashboard)
-router.route("/pay/:courseID/:userID").post(pay)
+router.route("/pay/:courseID/:userID").post(protect,pay)
 router.route("/verify-payment/:courseID/:userID/:id").get(verifyPayment)
 router.route("/payment-success").get(success)
+
 module.exports = router;
