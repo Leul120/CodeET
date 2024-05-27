eac751f7cd0033c538425e0a503dbe981aa40897
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-05-26 21:30:22 -0700

    Update userRoute.js

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,6 +1,6 @@
 const express=require('express')
 const { postUser, getUsers, getUser } = require('../controllers/userController')
-const { signup,login, verifyEmail } = require('../controllers/authController')
+const { signup,login, verifyEmail, logout } = require('../controllers/authController')
 const router=express.Router()
 
 router.route('/users').post(postUser).get(getUsers)
@@ -9,6 +9,7 @@ router.route('/users/signup').post(signup)
 router.route('/users/login').post(login)
 router.route('/users/forget-password').post(login)
 router.route('/verify-email').post(verifyEmail)
+router.route('/users/logout/:userID').get(logout)
 router.get('/', (req, res) => {
   res.status(200).json({
     status: 'success',
