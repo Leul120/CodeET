969a0cb8cf52fba89fbf12dbefd4e688615f12d0
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-05-26 21:48:30 -0700

    Update Login.js

--- client/src/Login.js
+++ client/src/Login.js
@@ -64,35 +64,7 @@ const Login = () => {
     }
 
   return (<>
-    <div className='justify-center flex items-center bg-gradient-to-t from-slate-950 to-slate-100 h-screen flex-col'>
-    <div className={`bg-gradient-to-r  mt-24 backdrop-blur-2xl h-full from-green-300 to-green-700 ${hide? "hidden":""}`}>
-    <p className={`text-black p-3 `}>
-    <p className='text-red-600 shadow-md shadow-red-600'>VERY CRUCIAL CONTENT.PLEASE READ IT CAREFULLY!!! </p>
-    <br />
-Access Policy <br />
-Welcome to our website. To ensure a secure and fair experience for all users, please adhere to the following access policies: <br /><br />
-
-Single Device Usage: <br />
-
-Please log in using the device you intend to use for watching the courses. Logging in on multiple devices is not permitted to prevent unauthorized sharing of purchased content. Access to the courses is limited to the device used to make the purchase. <br /><br />
-
-Avoid Logging Out: <br />
-
-Once you log out of your account, you will need to contact our support team to regain access. This process may take some time, so we strongly recommend that you do not log out. You will remain logged in on this device to avoid any interruptions. <br /><br />
-
-Maintaining Integrity: <br />
-
-These policies are in place to protect the integrity of our courses and to ensure that only paying users have access to the content. <br /><br />
-
-Support and Assistance: <br />
-
-If you have any questions or concerns regarding your access or encounter any issues, please do not hesitate to reach out to our support team. We are here to help you and ensure a smooth experience. <br /><br />
-
-We appreciate your understanding and cooperation in adhering to these policies. Thank you for helping us maintain a fair and secure learning environment.</p>
-<button className='bg-white rounded-2xl shadow-lg shadow-slate-800 float-end mr-16 mb-4 p-1' onClick={()=>{
-  setHide(true)
-}}>Accept</button>
-</div>
+    <div className='justify-center flex items-center bg-gradient-to-t from-slate-950 to-slate-100 h-screen'>
         <form onSubmit={handleSubmit(submitData)} className={`border  rounded-lg p-10 flex flex-col shadow-slate-800 shadow-lg border-border bg-gradient-to-t from-slate-200 to-slate-600 ${!hide? "hidden":""}`}>
         
           <p className='text-red-800 '>{errored}</p>
