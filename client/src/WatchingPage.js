34917fc62ddc3e1e503399be8564adc32e64ea0f
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-05-27 09:18:57 -0700

    Update WatchingPage.js

--- client/src/WatchingPage.js
+++ client/src/WatchingPage.js
@@ -7,79 +7,45 @@ import { useParams } from 'react-router-dom'
 import axios from 'axios'
 import { AppContext } from './App'
 import { useQuery } from 'react-query'
-
 import { useNavigate } from 'react-router-dom'
+
+
 const WatchingPage = () => {
-  const {enrolled,user}=useContext(AppContext)
-    const {courseID}=useParams()
-    const [course,setCourse]=useState({})
-    const navigate=useNavigate()
-    const videoRef = useRef(null);
-   
-    let token=atob(window.localStorage.getItem('token'))
-    useEffect(()=>{
-      if(!user || !enrolled){
-        navigate('/')
-      }
-    })
-    
-    const filter=async ()=>{
-      
-      const response=await axios.get(`${process.env.REACT_APP_URL}/api/${courseID}`,{withCredentials: true,
-        headers:{
-          Authorization:`Bearer ${token}`
-        }}
-      )
-      return response.data.courses
+  const [data,setData]=useState({})
+  const navigate=useNavigate()
+  const {user,enrolled}=useContext(AppContext)
+  if(!user || !enrolled){
+    navigate('/')
+  }
+  const courseID="FURNITURE"
+  const find=async ()=>{
+    try{
+    const response=await axios.get(`${process.env.REACT_APP_URL}/api/course/get-course/${courseID}`)
+      setData(response.data)
+    }catch(error){
+      console.log(error)
     }
-        const {data,error,isFetched}=useQuery("singleCourse",filter)
-        useEffect(()=>{
-          if(data){
-          setCourse(data)}},[setCourse,data])
+  }
 
-        
-        const [isVisible, setIsVisible] = useState(false);
-
-        useEffect(() => {
-          const handleScroll = () => {
-            if (videoRef.current) {
-              const rect = videoRef.current.getBoundingClientRect();
-              const isInViewport =
-                rect.top >= 0 &&
-                rect.left >= 0 &&
-                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-                rect.right <= (window.innerWidth || document.documentElement.clientWidth);
-              setIsVisible(isInViewport);
-            }
-          };
-      
-          window.addEventListener('scroll', handleScroll);
-      
-          return () => {
-            window.removeEventListener('scroll', handleScroll);
-          };
-        }, []);
-           
-             if(error?.request.status===401 || error?.request.status===500 ){
-              navigate('/login')
-              return(
-              <>
-                <h1 className='text-red-400 text-center'>{error.response.data.message} </h1>
-                
-              </>
-            )
-            }
-            else if(error?.code==='ERR_NETWORK'){
-              return(
-              <h1 className='text-red-500 text-center pt-5'>Internal Server Error</h1>)
-            }
-              else if(isFetched){
+  const mapper=()=>{
+    data.map((coursed,index)=>{
+      const course=coursed.key.split('/')
+      console.log(course[1])
+      return(
+        <div>{course[1]}</div>
+      )
+    })
+  }
+useEffect(()=>{
+  find()
+  mapper()
+},[])
   return (
     <div className=' pt-16 flex justify-center min-h-screen bg-gradient-to-t from-slate-950 to-slate-100' >
         
         <div className='w-full video'>
         <video
-      ref={videoRef}
+      
       controls
       disablePictureInPicture
       disableRemotePlayback
@@ -98,6 +64,5 @@ const WatchingPage = () => {
         <h1 className='text-blue-600 text-2xl mt-32'>{course.Title}</h1>
     </div>
   )
-}
 }
 export default WatchingPage
