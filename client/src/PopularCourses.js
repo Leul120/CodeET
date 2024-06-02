0b9da6ef3a6881bfdbaf5b2f5a79137b8fda1cbb
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-06-02 06:06:45 -0700

    Update PopularCourses.js

--- client/src/PopularCourses.js
+++ client/src/PopularCourses.js
@@ -48,7 +48,7 @@ const PopularCourses = () => {
           
           <Menu onChange={onChange} value={sort} onSelect={onChange} className='text-white bg-transparent '>
          
-          <Menu.SubMenu key="odd" title="sort" className='text-black  mt-10 border border-red-300 shadow-md shadow-red-500  '>
+          <Menu.SubMenu key="odd" title="sort" className='text-black  mt-10 border  shadow-md   '>
             <Menu.SubMenu  title="Released">
             <Menu.Item key="Released" className='text-2xl w-6 text-center'><IoIosArrowRoundUp/></Menu.Item>
             <Menu.Item key='-Released' className='text-2xl w-6 text-center' ><IoIosArrowRoundDown /></Menu.Item>
@@ -67,7 +67,7 @@ const PopularCourses = () => {
 
           
         </div>
-        <section className='pl-16 sm:pl-24 bg-gradient-to-t from-slate-700 to-slate-100'>
+        <section className='pl-16 sm:pl-24 bg-slate-300'>
     <Description/>
    
     <div className='xs:grid gap-9 xs:gap-5 flex flex-row flex-wrap xs:grid-cols-3  sm:grid-cols-3 md:grid-cols-3  lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-5 p-4 '>
@@ -76,10 +76,10 @@ const PopularCourses = () => {
             return(
                 // <div className='p-1 mt-1 w-44 h-48 xs:h-64 xs:w-auto border-spacing-1 border border-border rounded-lg bg-slate-800 shadow-md shadow-slate-600 flex justify-center object-fill'>
                 <Link to={`/course/${course._id}`} className='flex flex-col pb-5 justify-between w-28 xs:w-auto '><div><img alt={course.Title} src={course.Poster} className='h-24 shadow-md hover:shadow-border hover:size-2xl xs:h-48 w-28 xs:w-96 rounded-lg' loading='lazy'/>
-                <p className=' h-5 text-wrap mr-1 pt-1 pb-1 text-sm overflow-hidden w-32 xs:w-full bg-gradient-to-r from-green-600 to-purple-600 text-transparent bg-clip-text' key={Math.random()}>{course.Title}</p>
-                <p className='text-red-300 text-sm bg-gradient-to-r from-stone-200 to-orange-400 text-transparent bg-clip-text' key={Math.random()}>Released: {date}</p>
-                <p className='text-red-200 text-sm' key={Math.random()}>Rating: {course.Rating}</p>
-                <p className='text-red-300 text-sm bg-gradient-to-r from-stone-200 to-orange-400 text-transparent bg-clip-text' key={Math.random()}>{course.bought} {" "} people bought</p>
+                <p className=' h-5 text-wrap mr-1 pt-1 pb-1 text-sm overflow-hidden w-32 xs:w-full bg-gradient-to-r from-green-600 to-purple-600 text-transparent bg-clip-text font-bold' key={Math.random()}>{course.Title}</p>
+                
+                <p className='text-red-200 text-sm bg-gradient-to-r from-green-600 to-purple-600 text-transparent bg-clip-text' key={Math.random()}>Rating: {course.Rating}</p>
+                <p className='text-red-300 text-sm bg-gradient-to-r from-green-600 to-purple-600 text-transparent bg-clip-text' key={Math.random()}>{course.bought} {" "} people bought</p>
                 </div>
                 <button type="button" className="text-white mb-1 bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 w-28 xs:w-full dark:focus:ring-cyan-800 text-xs xs:font-medium xs:rounded-lg rounded-md px-5 py-2.5 h-6 xs:h-auto   me-2 pt-1  xs:text-center xs:text-nowrap overflow-hidden max-w-96"> {course.Price-0.01}Birrs only</button>
 
@@ -89,7 +89,7 @@ const PopularCourses = () => {
             )
         })}</div>
     
-        <Pagination count={4}  color='primary' page={page} shape="rounded" className='m-3  p-0  rounded-lg bg-gradient-to-r from-gray-300 to-slate-950 flex justify-center' onChange={async (e,i)=>{
+        <Pagination count={4}  color='primary' page={page} shape="rounded" className='m-3  p-0  rounded-lg bg-transparent flex justify-center' onChange={async (e,i)=>{
            await setPage(i)
            setRefetcher(true)
            
