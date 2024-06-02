6e56720dc8611288cac5308f9d7dd498222cc604
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-06-02 05:24:55 -0700

    Update SubfolderList.js

--- client/src/SubfolderList.js
+++ client/src/SubfolderList.js
@@ -8,6 +8,7 @@ const SubfolderList = () => {
   const [data, setData] = useState([]);
   const [courseUrl, setCourseUrl] = useState("");
   const { user, enrolled } = useContext(AppContext);
+  const [courseTitle,setCourseTitle]=useState("")
   const navigate = useNavigate();
   const { folderName } = useParams();
   const courseID = "nnn";
@@ -31,6 +32,7 @@ const SubfolderList = () => {
   }, [courseID]);
 
   const handleItemClick = (cour) => {
+    setCourseTitle(cour.key.split('/')[2].split('.')[1])
     setCourseUrl(cour.url);
   };
 
@@ -49,11 +51,12 @@ const SubfolderList = () => {
           style={{height:"10rem"}}
           config={{ file: { attributes: { controlsList: 'nodownload' } } }}
         />
+        <h1 className="font-bold p-2">{courseTitle}</h1>
       </div>
-      <ul className=''>
+      <ul className='bg-slate-100'>
         {filtered.map((cour, index) => (
-          <li key={index} className='shadow my-1 hover:bg-blue-600  hover:text-white h-10 flex items-center cursor-pointer' onClick={() => handleItemClick(cour)}>
-            {cour.key.split('/')[2]}
+          <li  key={index} className='shadow my-1 p-1 hover:bg-blue-600  hover:text-white h-10 flex items-center cursor-pointer' onClick={() => handleItemClick(cour)}>
+           <p className="p-1 mr-1 rounded-3xl text-center bg-slate-300 w-8 h-8">{cour.key.split('/')[2].split('.')[0]}</p> {cour.key.split('/')[2].split('.')[1]}
           </li>
         ))}
       </ul>
