3492a3cf488fc952d14e21a39ff816f027777764
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-06-05 16:37:42 -0700

    Update PopularCourses.js

--- client/src/PopularCourses.js
+++ client/src/PopularCourses.js
@@ -7,7 +7,7 @@ import Description from './Description'
 import Footer from './Footer'
 import Pagination from '@mui/material/Pagination';
 import {LoadingOutlined} from '@ant-design/icons'
-import { Menu } from 'antd'
+import { Menu, Rate } from 'antd'
 
 import { IoIosArrowRoundDown, IoIosArrowRoundUp } from 'react-icons/io'
 
@@ -64,8 +64,6 @@ const PopularCourses = () => {
           </Menu.SubMenu>
        
         </Menu>
-
-          
         </div>
         <section className='pl-16 sm:pl-24 bg-slate-100'>
     <Description/>
@@ -74,11 +72,10 @@ const PopularCourses = () => {
         {courses?.map((course,index)=>{
             const date= course?.Released.slice(0,4)
             return(
-                // <div className='p-1 mt-1 w-44 h-48 xs:h-64 xs:w-auto border-spacing-1 border border-border rounded-lg bg-slate-800 shadow-md shadow-slate-600 flex justify-center object-fill'>
                 <Link to={`/course/${course._id}`} className='flex flex-col pb-5 justify-between w-28 xs:w-auto '><div><img alt={course.Title} src={course.Poster} className='h-24 shadow-md hover:shadow-border hover:size-2xl xs:h-48 w-28 xs:w-96 rounded-lg' loading='lazy'/>
                 <p className=' h-5 text-wrap mr-1 pt-1 pb-1 text-sm overflow-hidden w-32 xs:w-full bg-gradient-to-r from-green-600 to-purple-600 text-transparent bg-clip-text font-bold' key={Math.random()}>{course.Title}</p>
                 
-                <p className='text-red-200 text-sm bg-gradient-to-r from-green-600 to-purple-600 text-transparent bg-clip-text' key={Math.random()}>Rating: {course.Rating}</p>
+                <p className='text-red-200 text-sm bg-gradient-to-r from-green-600 to-purple-600 text-transparent bg-clip-text' key={Math.random()}><Rate disabled allowHalf defaultValue={course.Rating/2} /> {course.Rating}</p>
                 <p className='text-red-300 text-sm bg-gradient-to-r from-green-600 to-purple-600 text-transparent bg-clip-text' key={Math.random()}>{course.bought} {" "} people bought</p>
                 </div>
                 <button type="button" className="text-white mb-1 bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 w-28 xs:w-full dark:focus:ring-cyan-800 text-xs xs:font-medium xs:rounded-lg rounded-md px-5 py-2.5 h-6 xs:h-auto   me-2 pt-1  xs:text-center xs:text-nowrap overflow-hidden max-w-96"> {course.Price-0.01}Birrs only</button>
