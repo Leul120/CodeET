a4a0f8effc4c8a52d18b3f6a882c282394d0bee3
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-06-08 08:57:34 -0700

    Update VerifyEmail.js

--- client/src/VerifyEmail.js
+++ client/src/VerifyEmail.js
@@ -1,16 +1,35 @@
-// import React, { useContext, useState } from 'react'
-// import { AppContext } from './App'
-// import { useNavigate, useSearchParams } from 'react-router-dom'
+import { message } from 'antd'
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { LoadingOutlined } from '@ant-design/icons';
+const VerifyEmail = () => {
+  const navigate=useNavigate()
+    const users=JSON.parse(window.localStorage.getItem("users"))
+    const token=useParams().token
+    console.log(users)
+    useEffect(()=>{
+        const verifier=async ()=>{
+        try{
+        const response=await axios.post(`http://localhost:8021/users/verifyEmail/${token}`,users)
+        console.log(response.data)
+        const custom=btoa(response.data.token)
+         window.localStorage.setItem('token',custom)
+         window.localStorage.setItem('user',JSON.stringify(response.data.newUser))
+        message.success("Email verified successfully!")
+        navigate('/')
+        }catch(error){
+          console.log(error)
+            message.error("error occured!")
+        }}
+        verifier()
+    },[])
 
-// const VerifyEmail = () => {
-//     const {user,updateUser,isLoading,seIsLoading}=useContext(AppContext)
-//     const [error,setError]=useState(false)
-//     const [searchParams,setSearchParams]=useSearchParams()
-//     const navigate=useNavigate()
-//     const emailToken=searchParams.get("emailToken")
-//   return (
-//     <div>VerifyEmail</div>
-//   )
-// }
+  return (
+    <div>
+      <div className='w-full  mt-64 flex items-center justify-center text-3xl'><LoadingOutlined spinning allowFullScreen size="large" style={{color:"black",font:50}}/></div>
+    </div>
+  )
+}
 
-// export default VerifyEmail
+export default VerifyEmail
