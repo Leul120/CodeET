0b886cfa6f49c69de39da54edf5f746a5eced841
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-06-08 00:03:57 -0700

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -7,12 +7,28 @@ const {promisify}=require('util')
 const crypto=require('crypto')
 const {authSchema}=require('../models/validateSchema')
 const UserModel = require('../models/UserModel')
+const Recipient = require("mailersend").Recipient;
+const EmailParams = require("mailersend").EmailParams;
+const MailerSend = require("mailersend");
+const bcrypt=require('bcrypt')
 const signToken=id=>{
     return jwt.sign({id:id},process.env.JWT_SECRET,{
         expiresIn:process.env.JWT_EXPIRES_IN
     })
 }
 const signRefreshToken=(id)=>{
+    return jwt.sign({id:id},process.env.JWT_SECRET,{
+        expiresIn:process.env.JWT_EXPIRES_IN
+    })
+}
+let transporter = nodemailer.createTransport({
+    service: 'gmail', // Example using Gmail
+    auth: {
+        user: 'codeetgo@gmail.com',
+        pass: "ajtr hgyi yqsz tkgv"
+    }
+});
+const createPasswordResetToken=(id)=>{
     return jwt.sign({id:id},process.env.JWT_SECRET,{
         expiresIn:process.env.JWT_EXPIRES_IN
     })
@@ -38,7 +54,7 @@ exports.signup=catchAsync(async (req,res,next)=>{
 exports.login=catchAsync(async (req,res,next)=>{
     const {email,password}=req.body 
     
-    const token=''
+    
    
     if(!email||!password){
         return next(new AppError('Please provide email and password!',400))
@@ -103,48 +119,74 @@ exports.restrictTo=(...roles)=>{
         next()
     }
 }
+
+const nodemailer = require('nodemailer');
+
+
+
+
 exports.forgetPassword=catchAsync(async(req,res,next)=>{
-    const user=await User.findOne({email:req.body.email})
+    console.log(req.body.email)
+    const user=await User.findOne({email:"melete@gmail.com"})
+    console.log("hello")
     if(!user){
         return next(new AppError('There is no user with this email address',404));
     }
-    const resetToken=user.createPasswordResetToken()
-    await user.save({validateBeforeSave:false})
+    const resetToken=createPasswordResetToken(user._id)
+    const hashedToken=crypto.createHash('sha256').update(token).digest('hex')
+    await User.findByIdAndUpdate(user._id, {
+        passwordResetToken: hashedToken,
+        passwordResetExpires: Date.now() + 10 * 60 * 1000 // Token expires in 10 minutes
+    });
+    let mailOptions = {
+    from: '"CodeET" <codeetgo@gmail.com>', // Sender address
+    to: req.body.email, // List of recipients
+    subject: 'Reset Your Password', // Subject line
+    text: 'Greetings from the team.', // Plain text body
+    html: `<p>Hi ${user.name},</p><p>We received a request to reset your password. Click the link below to reset it:</p><button><a href="http://code-et.vercel.app/resetPassword/${resetToken}">Reset Password</a></button> <p>If you didn't request a password reset, please ignore this email.</p><p>Thanks, Team</p>`
+};
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        res.status(400).json({
+            status:400,
+            message:"An Error Occured"})
+        console.log(error)
+    }
+    console.log('Email sent successfully:', info.response);
+    res.status(200).json({status:200,
+        message:"Email sent successfully, Please check your email to verify and please check the spams folder in your email if you don't find it in your inbox"})
+});
 })
-exports.resetPassword=catchAsync(async(req,res,next)=>{
-const hashedToken=crypto.createHash('sha256').update(req.params.token).digest('hex')
-const user=await User.findOne({passwordResetToken:hashedToken,passwordResetExpires:{$gt:Date.now()}})
-if(!user){
-    return next(new AppError('Token is invalid or has expired',401))
-}
-user.password=req.body.password
-user.passwordConfirm=req.body.passwordConfirm
-user.passwordResetToken=undefined
-user.passwordResetExpires=undefined
-await user.save()
-})
-exports.verifyEmail=async (req,res)=>{
-    try{
-        const emailToken=req.body.emailToken;
-        if(!emailToken)return res.status(404).json("Email token not found...")
-            const user=await User.findOne({emailToken});
-        if(user){
-            user.emailToken=null;
-            user.isVerified=true;
-            await user.save()
-            const token=signToken(user._id);
-            res.status(200).json({
-                _id:user._id,
-                name:user.name,
-                email:user.email,
-                token,
-                isVerified:user?.isVerified
-            })
-        }else{
-            res.status(404).json("Email verification failed,invalid token!")
-        }
-    }catch(error){
-     
-            res.status(500).json(error.message)
-        }
-}
+
+exports.resetPassword=catchAsync(async (req,res,next)=>{
+        const token=req.params.resetToken
+         const hashedToken=crypto.createHash('sha256').update(token).digest('hex')
+         const user=await User.findOne({passwordResetToken:hashedToken})
+         if(!user){
+            return next(AppError('Token is invalid or has expired', 400))
+         }
+        
+        
+        res.status(200).json({
+            status: 200,
+            message: 'Email verified successfully'
+        });
+
+    })
+exports.updateForgottenPassword=catchAsync(async(req,res)=>{
+    const user=await User.findOne({email:req.params.email})
+    user.password = await bcrypt.hash(req.body.newPassword, 12);
+        user.passwordResetToken = undefined;
+        user.passwordResetExpires = undefined;
+        await user.save();
+        res.status(200).json({message:"Password Reset Successfully"})
+})     
+   
+
+
+
+
+
+
+
+
