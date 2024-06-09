0493ad6034b563258670098aba5f8375526f3de4
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-06-09 08:53:45 -0700

    Update VerifyEmail.js

--- client/src/VerifyEmail.js
+++ client/src/VerifyEmail.js
@@ -1,4 +1,4 @@
-import { Input, message } from 'antd'
+import { Input, message,notification } from 'antd'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
@@ -6,6 +6,12 @@ import { LoadingOutlined } from '@ant-design/icons';
 const VerifyEmail = () => {
   const navigate=useNavigate()
     const newID=useParams().newID
+  const [api, contextHolder] = notification.useNotification();
+     api.open({
+      message: 'Email sent',
+      description:`Please check your email to verify and please check the spams folder in your email if you don't find it in your inbox`,
+      duration: 0,
+    });
     const [verificationCode,setVerificationCode]=useState("")
   
         const verifier=async ()=>{
@@ -19,12 +25,13 @@ const VerifyEmail = () => {
         navigate('/')
         }catch(error){
           console.log(error)
-            message.error("error occured!")
+            message.error("error occurred!")
         }}
        
 
   return (
     <div className='flex h-screen justify-center flex-col items-center bg-slate-200'>
+    {contextHolder}
       <Input placeholder='Your verification code' className='w-44 my-3' onChange={(e)=>{
         setVerificationCode(e.target.value)
       }}/>
