311195ca1446239db63e1b758582bb50ec922581
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-06-09 00:40:29 -0700

    Update VerifyEmail.js

--- client/src/VerifyEmail.js
+++ client/src/VerifyEmail.js
@@ -1,21 +1,20 @@
-import { message } from 'antd'
+import { Input, message } from 'antd'
 import axios from 'axios'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { LoadingOutlined } from '@ant-design/icons';
 const VerifyEmail = () => {
   const navigate=useNavigate()
-    const users=JSON.parse(window.localStorage.getItem("users"))
-    const token=useParams().token
-    console.log(users)
+    const newID=useParams().newID
+    const [verificationCode,setVerificationCode]=useState("")
     useEffect(()=>{
         const verifier=async ()=>{
         try{
-        const response=await axios.post(`${process.env.REACT_APP_URL}/users/verifyEmail/${token}`,users)
+        const response=await axios.post(`${process.env.REACT_APP_URL}/users/verifyEmail/${newID}`,{verificationCode:verificationCode})
         console.log(response.data)
         const custom=btoa(response.data.token)
          window.localStorage.setItem('token',custom)
-         window.localStorage.setItem('user',JSON.stringify(response.data.newUser))
+         window.localStorage.setItem('user',JSON.stringify(response.data.user))
         message.success("Email verified successfully!")
         navigate('/')
         }catch(error){
@@ -26,8 +25,11 @@ const VerifyEmail = () => {
     },[])
 
   return (
-    <div>
-      <div className='w-full  mt-64 flex items-center justify-center text-3xl'><LoadingOutlined spinning allowFullScreen size="large" style={{color:"black",font:50}}/></div>
+    <div className='flex justify-center'>
+      <Input variant='borderLess' className='w-44' onChange={(e)=>{
+        setVerificationCode(e.target.value)
+      }}/>
+      <Input type='submit' value='Submit' className='w-32'/>
     </div>
   )
 }
