ed601e163886d390c29225be1e28927e99f6324b
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-06-09 02:54:30 -0700

    Update authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -50,6 +50,7 @@ exports.signup=catchAsync(async (req,res,next)=>{
     await User.findOneAndUpdate({_id:newUser._id},{token:token})
     if(new Date(newUser.created_at)>=new Date(Date.now()-2*60*1000) &&!newUser.isVerified){
         await User.findOneAndDelete({_id:newUser._id})
+        console.log("deleted")
     }
     let mailOptions = {
     from: '"CodeET" <codeetgo@gmail.com>', // Sender address
