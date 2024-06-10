72c16a2aeb4b8698f50073eb3f7950a5ea4bb04a
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-06-10 00:19:53 -0700

    Update Footer.js

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className='relative '>
         
 
-<footer class="  shadow-md  shadow-slate-800">
+<footer class=" ">
     <div class="mx-auto w-full max-w-screen-xl ">
       <div class="grid grid-cols-2 gap-8 px-4 py-6 lg:py-8 md:grid-cols-3">
         <div>
