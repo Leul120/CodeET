640b169b3f24784f5493c832e72e967b0ccd1083
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-06-09 17:48:37 -0700

    Update ResetPassword.js

--- client/src/ResetPassword.js
+++ client/src/ResetPassword.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { AppContext } from './App'
 import { Input } from 'antd'
 import { message } from 'antd'
 import { useNavigate } from 'react-router-dom'
