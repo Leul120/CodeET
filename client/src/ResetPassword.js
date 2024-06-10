ce479dae2fd131edb5cd1dfaaeb947e0d2359bcc
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-06-09 23:59:35 -0700

    Update ResetPassword.js

--- client/src/ResetPassword.js
+++ client/src/ResetPassword.js
@@ -46,14 +46,14 @@ const ResetPassword = () => {
   return (
     <div>
         {status===200?(
-            <div className='flex justify-center bg-stone-100'>
-            <div className='mt-44 w-96 flex flex-col bg-shite justify-center gap-4 p-10 '>
+            <div className='flex justify-center bg-stone-100 h-screen'>
+            <div className='mt-44 w-96 flex flex-col bg-white rounded-3xl  justify-center gap-4 p-10 '>
             <Input  placeholder='your new password' className='rounded-md' onChange={(e)=>{
                 console.log(e.target.value)
                 setPassword(e.target.value)
             }}/>
-            {!loading&&( <button  type='submit' value="Submit" onClick={updatePassword} className='bg-indigo-500 h-9 text-white hover:bg-white hover:text-indigo-500 text-serif text-sm' >Reset</button>)}
-            {loading&&(<button  type='submit' disabled value="Submit" onClick={updatePassword} className='bg-indigo-500 h-9 text-white flex flex-row justify-center items-center gap-2 text-serif text-sm' ><LoadingOutlined spinning allowFullScreen size="large" style={{color:"white"}}/>Reseting</button>)}
+            {!loading&&( <button  type='submit' value="Submit" onClick={updatePassword} className='bg-indigo-500 h-9 text-white hover:bg-white hover:text-indigo-500 text-serif text-sm rounded-3xl' >Reset</button>)}
+            {loading&&(<button  type='submit' disabled value="Submit" onClick={updatePassword} className='bg-indigo-500 h-9 text-white flex flex-row justify-center items-center gap-2 rounded-3xl text-serif text-sm' ><LoadingOutlined spinning allowFullScreen size="large" style={{color:"white"}}/>Reseting</button>)}
             </div></div>
         ):(<p className='text-center'>Verification Failed</p>)}
     </div>
