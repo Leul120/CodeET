11c205d822fc1afc6b2192343e55c00a9b53e2d3
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-06-09 17:48:21 -0700

    Update ForgetPassword.js

--- client/src/ForgetPassword.js
+++ client/src/ForgetPassword.js
@@ -1,20 +1,23 @@
 import axios from 'axios'
 import React, { useContext, useState } from 'react'
 import { AppContext } from './App'
-import { Input,notification } from 'antd'
-
+import { Button, Input,notification } from 'antd'
+import { LoadingOutlined } from '@ant-design/icons';
 const ForgetPassword = () => {
 
   const {email,setEmail}=useContext(AppContext)
-  const [status,setStatus]=useState('')
+  const [setStatus]=useState('')
   const [api, contextHolder] = notification.useNotification();
+  const [loading,setLoading]=useState(false)
   const submitEmail=async ()=>{
     window.localStorage.setItem('resetEmail',email)
     console.log(email)
     try{
+      setLoading(true)
     const response=await axios.post(`${process.env.REACT_APP_URL}/users/forgetPassword`,{email:email})
     console.log(response.data)
     setStatus(response.data.message)
+    setLoading(false)
     api.open({
       message: 'Email sent',
       description:response.data.message,
@@ -22,19 +25,22 @@ const ForgetPassword = () => {
     });
     
   }catch(error){
+    setLoading(false)
     console.log(error)
   }
   }
   
   return (
-    <div className='flex flex-col justify-center items-center h-screen'>
+    <div className='flex flex-col justify-center items-center bg-stone-100 h-screen'>
     {contextHolder}
-        <div  className='rounded-lg bg-slate-100 w-1/2 shadow-slate-400 shadow-md border flex flex-col justify-center  border-border p-10 h-56'>
-        <Input type="text" placeholder='email@gmail.com' className='rounded-lg mb-3'onChange={(e)=>{
+        <div  className='rounded-3xl bg-white w-1/2  shadow-md  flex flex-col justify-center   p-10 h-56'>
+        <h1 className='text-serif font-bold text-sky-500'>Verify email</h1>
+        <Input type="text" placeholder='email@gmail.com' className='rounded-lg mb-3 mt-3'onChange={(e)=>{
           console.log(e.target.value)
           setEmail(e.target.value)
-        }}/>
-        <Input type="submit" value='Send Verification Link'  onClick={submitEmail}/></div>
+        }}/>{loading &&(<button type="submit" value='Send Verification Link' className='bg-indigo-300 h-9 rounded-3xl text-white   text-sm text-serif  flex flex-row gap-2 justify-center items-center' disabled ><LoadingOutlined spinning allowFullScreen size="large" style={{color:"white"}}/>Send Verification Link</button>)}
+        {!loading &&(<button type="submit" value='Send Verification Link' className='bg-indigo-500 h-9 rounded-3xl text-white hover:text-indigo-500 hover:bg-white text-sm text-serif hover:border'  onClick={submitEmail}>Send Verification Link</button>)}
+        </div>
     </div>
   )
 }
