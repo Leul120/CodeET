9820c4c97d5656da0ddfeb91456193fe1ad7a2d8
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-06-09 17:48:45 -0700

    Update VerifyEmail.js

--- client/src/VerifyEmail.js
+++ client/src/VerifyEmail.js
@@ -6,6 +6,7 @@ import { LoadingOutlined } from '@ant-design/icons';
 const VerifyEmail = () => {
   const navigate=useNavigate()
     const newID=useParams().newID
+    const [loading,setLoading]=useState(false)
     const [api, contextHolder] = notification.useNotification();
     const lll= ()=>{
       
@@ -15,23 +16,26 @@ const VerifyEmail = () => {
       duration: 0,
     });
     }
-    useEffect(()=>{
-      lll()
-    },[])
+    // useEffect(()=>{
+    //   lll()
+    // },[])
   
      
     const [verificationCode,setVerificationCode]=useState("")
   
         const verifier=async ()=>{
         try{
+          setLoading(true)
         const response=await axios.post(`${process.env.REACT_APP_URL}/users/verifyEmail/${newID}`,{verificationCode:verificationCode})
         console.log(response.data)
+        setLoading(false)
         const custom=btoa(response.data.token)
          window.localStorage.setItem('token',custom)
          window.localStorage.setItem('user',JSON.stringify(response.data.user))
         message.success("Email verified successfully!")
         navigate('/')
         }catch(error){
+          setLoading(false)
           console.log(error)
             
             api.open({
@@ -43,12 +47,16 @@ const VerifyEmail = () => {
        
 
   return (
-    <div className='flex h-screen justify-center flex-col items-center bg-slate-200'>
+    <div className='flex h-screen justify-center flex-col items-center bg-stone-100'>
     {contextHolder}
-      <Input placeholder='Your verification code' className='w-44 my-3' onChange={(e)=>{
+    <div className='flex flex-col bg-white p-10 justify-center rounded-3xl shadow-lg '>
+    <h1 className='text-serif font-bold text-sky-500 pb-5'>Verify email</h1>
+      <Input variant='borderLess' placeholder='Your verification code'  className='w-48 ml-3  border-0 underline decoration-indigo-500 underline-offset-2 focus:underline-offset-6' onChange={(e)=>{
         setVerificationCode(e.target.value)
       }}/>
-      <Input type='submit' value='Submit' className='w-32' onClick={verifier}/>
+      {!loading &&(<button type='submit' value='Submit' className='w-32 mt-3 ml-9 bg-indigo-500 text-serif text-white rounded-3xl h-9 text-sm' onClick={verifier}>Submit</button>)}
+      {loading &&(<button type='submit' value='Submit' className='w-32 mt-3 ml-9 bg-indigo-500 text-serif text-white rounded-3xl h-9 text-sm flex flex-row justify-center items-center gap-2 ' onClick={verifier}><LoadingOutlined spinning allowFullScreen size="large" style={{color:"white"}}/>Verifying</button>)}
+      </div>
     </div>
   )
 }
