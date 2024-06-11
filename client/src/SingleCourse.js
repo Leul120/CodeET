d17857a1aa4be1699b86c9c5a107a4b28015e4db
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-06-10 18:43:53 -0700

    Update SingleCourse.js

--- client/src/SingleCourse.js
+++ client/src/SingleCourse.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState} from 'react';
 import { AppContext } from './App';
 import { useNavigate, useParams } from 'react-router-dom';
-
+import './popular.css'
 import axios from 'axios';
 
 import { LoadingOutlined } from '@ant-design/icons';
@@ -71,7 +71,7 @@ window.localStorage.setItem('user',JSON.stringify(response.data.user))
   return (
     <div>
       {isLoading? (<div className='w-full h-1/2 mt-64 flex items-center justify-center text-3xl'><LoadingOutlined spinning allowFullScreen size="large" style={{color:"black",font:50}}/></div>):(
-    <div className='flex flex-row pt-10 items-center flex-wrap bg-stone-100 pl-3 min-h-screen'>
+    <div className='flex flex-row pt-10 items-center flex-wrap backdrop-blur-sm pl-3 min-h-screen single'>
     
 
     
