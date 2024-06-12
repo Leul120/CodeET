4d3397c086cc104b2ff71a2d3193f93d26cbb2df
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-06-12 07:34:45 -0700

    Update Description.js

--- client/src/Description.js
+++ client/src/Description.js
@@ -5,7 +5,7 @@ import React from 'react'
 
 return(
   <>
-  <h1 className='bg-gradient-to-r from-green-600 to-purple-200 text-transparent bg-clip-text text-6xl font-serif  text-center font-extrabold font flex justify-center '><span className=" ">Code</span><span className="">ET</span></h1>
+  <h1 className='bg-gradient-to-r from-purple-600 to-blue-500 text-transparent bg-clip-text text-6xl   text-center font-extrabold font flex justify-center '><span className=" ">Code</span><span className="">ET</span></h1>
   <p className=' text-stone-300 backdrop-blur-lg m-2 rounded-2xl p-3  text-serif  text-sm'>
   Welcome to our platform, where we are dedicated to empowering the code enthusiasts of Ethiopia. As passionate believers in the transformative power of education, we are proud to offer Udemy courses at a fraction of their original price. These courses, carefully curated from Udemy's vast library, are the best of the best, handpicked to cater to the needs and aspirations of our youth. Our mission is to make high-quality programming and web development training accessible to everyone, fostering a new generation of skilled and confident coders who will lead Ethiopia's digital revolution. Explore our extensive course catalog, and unlock your true potential at a price that fits your budget.
   </p>
