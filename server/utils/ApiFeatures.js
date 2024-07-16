5c26401c854dda2876970a3e1fa54e81c79ed71d
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-07-16 15:19:08 -0700

    Update ApiFeatures.js

--- server/utils/ApiFeatures.js
+++ server/utils/ApiFeatures.js
@@ -32,7 +32,7 @@ class ApiFeatures{
     }
     pagination(){
       const page=this.queryString.page*1 ||1;
-        const limit=this.queryString.limit*1||10
+        const limit=this.queryString.limit*1||8
         const skip=(page-1)*limit
         this.query=this.query.skip(skip).limit(limit)
         
