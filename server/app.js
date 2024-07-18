cd1ba6997478de51f62b04bfe3294d8b3bf08d01
Leul120 <140542389+Leul120@users.noreply.github.com>
2024-07-18 01:30:01 -0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ const connectDB = require('./db/connect');
 const bodyParser=require('body-parser')
 const AppError =require('./utils/appError')
 const GlobalErrorHandler=require('./controllers/ErrorController')
+const rateLimit=require('express-rate-limit')
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
@@ -23,8 +24,14 @@ app.use(cors({
   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Authorization','Accept'],
   credentials: true,
 }));
-
-
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Redis, Memcached, etc. See below.
+})
+app.use(limiter)
 app.use('/api',courseRoute)
 app.use('/',userRoutes)
 app.all('*',(req,res,next)=>{
